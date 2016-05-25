e9067376f015de7cc611ae0ae4bc7c6ecafedd76
yangyonghui <yangyonghui@netposa.com>
2016-05-25 12:07:56 +0800

    Initial Commit

---
+++ UIViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIViewDemo
+//
+//  Created by 阳永辉 on 16/5/25.
+//  Copyright © 2016年 netposa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UIViewDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UIViewDemo
+//
+//  Created by 阳永辉 on 16/5/25.
+//  Copyright © 2016年 netposa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
